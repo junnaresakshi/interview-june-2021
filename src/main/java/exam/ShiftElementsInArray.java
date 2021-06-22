8f004f7def0509285fc1376943577f49e0cf1143
junnaresakshi <71630648+junnaresakshi@users.noreply.github.com>
2021-06-22 12:51:18 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -4,7 +4,26 @@ public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
+    int n=1;
+    for(int i=0;i<arr.length;i++)
+    {
+      St=ystem.out.print(arr[i]+",");
+    }
+    for(int j=0;j<n;j++)
+    {
+      int k,last;
+      last=arr[arr.length-1];
+      for(k=arr.length-1;k>0;k--)
+      {
+        arr[k]=arr[k-1];
+    }
+arr[0]=last;
   }
+    System.out.println();
+    for(int i=0;i<arr.length;i++)
+    {
+      System.out.print(arr[i]+",");
+    }
 
+}
 }
