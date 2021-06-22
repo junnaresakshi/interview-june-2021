d49252b4fe098b2c1d094c7b9b1bb612fc091a8c
junnaresakshi <71630648+junnaresakshi@users.noreply.github.com>
2021-06-22 12:31:25 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,23 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    if(n<0)
+    {
+      return "Enter Positive number."
+    }
+    else
+    {
+      char[] bino=new char[32];
+      int i=0;
+      while(n>0)
+      {
+        bino[i]=n%2;
+        n=n/2;
+        i++;
+      }
+      StringBuffer sb=new StringBuffer(bino);
+      sb.reverse();
+      return sb;
+    }
   }
 }
